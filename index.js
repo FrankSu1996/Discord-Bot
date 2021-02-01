a7341f44443a931e4e7b13a457f07c75ddfa8b8a
Frank Su <franksu49@gmail.com>
2021-01-31 23:56:20 -0500

    set chrome options to use user data copied over

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ dotenv.config();
 
 // set chrome to use default chrome profile
 let options = new chrome.Options();
-
+options.addArguments("--user-data-dir=/Users/frank/Documents/ChromeProfile");
+options.addArguments("start-maximized");
 // instantiate chrome driver
 const driver = new Builder()
   .setChromeOptions(options)
@@ -17,19 +18,50 @@ const driver = new Builder()
   .build();
 
 // clicks a server button
-let clickServer = async (driver, serverName) => {
-  await driver.findElement(By.xpath(XPATHS.GET_SERVER_XPATH(serverName)));
+let clickServer = async (serverName) => {
+  await driver
+    .findElement(By.xpath(XPATHS.HOME_PAGE.GET_SERVER_XPATH(serverName)))
+    .click();
+  console.log("Clicked server" + serverName);
 };
 
-let login = async (driver, userName, password) => {
-  await driver;
+let login = async () => {
+  await driver
+    .findElement(By.xpath(XPATHS.LOGIN_PAGE.EMAIL_INPUT))
+    .sendKeys("franksu49@gmail.com");
+  await driver
+    .findElement(By.xpath(XPATHS.LOGIN_PAGE.PASSWORD_INPUT))
+    .sendKeys(process.env.password);
+  await driver.findElement(By.xpath(XPATHS.LOGIN_PAGE.LOGIN_BUTTON)).click();
+  await driver.wait(
+    until.elementLocated(
+      By.xpath(XPATHS.HOME_PAGE.GET_SERVER_XPATH("BouncyBalls")),
+      10000
+    )
+  );
+  console.log("Successfully logged in");
+};
+
+let handleVoiceAlert = async () => {
+  while (true) {
+    try {
+      await driver.switchTo().alert().accept();
+      break;
+    } catch (e) {
+      console.log("Can't find alert");
+      await driver.sleep(1000);
+    }
+  }
 };
 
 client.once("ready", async () => {
   await driver.get("https://discord.com/login");
-  await driver.manage().window().maximize();
 
-  console.log("Ready!");
+  await driver.sleep(5000);
+  await clickServer("BouncyBalls");
+  await driver
+    .findElement(By.xpath(XPATHS.HOME_PAGE.GET_VOICE_CHANNEL_XPATH("General")))
+    .click();
 });
 
 client.login(process.env.TOKEN);
--- xpaths.js
+++ xpaths.js
@@ -1,10 +1,24 @@
 const XPATHS = {
-  GET_SERVER_XPATH: function (serverName) {
-    return (
-      "//div[@aria-label='Servers']/descendant::div[contains(@aria-label,'" +
-      serverName +
-      "')]"
-    );
+  LOGIN_PAGE: {
+    EMAIL_INPUT: "//input[@aria-label='Email or Phone Number']",
+    PASSWORD_INPUT: "//input[@aria-label='Password']",
+    LOGIN_BUTTON: "//button[@type='submit']",
+  },
+  HOME_PAGE: {
+    GET_SERVER_XPATH: function (serverName) {
+      return (
+        "//div[@aria-label='Servers']/descendant::div[contains(@aria-label,'" +
+        serverName +
+        "')]"
+      );
+    },
+    GET_VOICE_CHANNEL_XPATH: function (voiceChannel) {
+      return (
+        "//div[@aria-label='Voice']/following-sibling::div[text()='" +
+        voiceChannel +
+        "']"
+      );
+    },
   },
 };
 
